250a8e9031ee8d4a72e66d44e26277252a7cf4fa
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-06-12 10:20:04 +0100

    Style cab show page

--- src/library/cabs.js
+++ src/library/cabs.js
@@ -5,6 +5,7 @@ const cabsData = [
     model: 'Honda City',
     transmission: 'Automatic',
     rentalPrice: 100,
+    discount: 5,
     imageUrl:
       'https://images.unsplash.com/photo-1600259828526-77f8617ceec9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aG9uZGElMjBjaXR5fGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60',
     seatingCapacity: 4,
@@ -18,6 +19,7 @@ const cabsData = [
     model: 'Ford Mustang',
     transmission: 'Automatic',
     rentalPrice: 100,
+    discount: 5,
     imageUrl:
       'https://images.unsplash.com/photo-1603553329474-99f95f35394f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Rm9yZCUyME11c3Rhbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60',
     seatingCapacity: 4,
@@ -31,6 +33,7 @@ const cabsData = [
     model: 'BMW 3 Series',
     transmission: 'Automatic',
     rentalPrice: 100,
+    discount: 5,
     imageUrl:
       'https://images.unsplash.com/photo-1613027570801-5d2fe8f5e15d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Qk1XJTIwMyUyMFNlcmllc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=400&q=60',
     seatingCapacity: 5,
@@ -44,6 +47,7 @@ const cabsData = [
     model: 'Toyota Highlander',
     transmission: 'Automatic',
     rentalPrice: 100,
+    discount: 5,
     imageUrl:
       'https://images.unsplash.com/photo-1610855143470-0967a7348972?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8VG95b3RhJTIwSGlnaGxhbmRlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=400&q=60',
     seatingCapacity: 7,
@@ -57,6 +61,7 @@ const cabsData = [
     model: 'Chevrolet Tahoe',
     transmission: 'Automatic',
     rentalPrice: 100,
+    discount: 5,
     imageUrl:
       'https://images.unsplash.com/photo-1564422426545-8a43cd6598ff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Q2hldnJvbGV0JTIwVGFob2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60',
     seatingCapacity: 8,
@@ -70,6 +75,7 @@ const cabsData = [
     model: 'Nissan Altima',
     transmission: 'Automatic',
     rentalPrice: 100,
+    discount: 5,
     imageUrl:
       'https://images.unsplash.com/photo-1581540222194-0def2dda95b8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Tmlzc2FuJTIwQWx0aW1hfGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60',
     seatingCapacity: 5,
@@ -83,6 +89,7 @@ const cabsData = [
     model: 'Mercedes-Benz C-Class',
     transmission: 'Automatic',
     rentalPrice: 100,
+    discount: 5,
     imageUrl:
       'https://images.unsplash.com/photo-1681170368080-f240d1216904?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fE1lcmNlZGVzJTIwQmVueiUyMEMlMjBDbGFzc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=400&q=60',
     seatingCapacity: 5,
--- src/pages/CabShow.jsx
+++ src/pages/CabShow.jsx
@@ -3,7 +3,11 @@ import { useParams, Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { BiLeftArrow } from 'react-icons/bi';
 import { BsCarFront } from 'react-icons/bs';
-import { MdOutlineFavoriteBorder } from 'react-icons/md';
+import {
+  MdOutlineFavoriteBorder,
+  MdOutlineKeyboardArrowRight,
+} from 'react-icons/md';
+import MultiColorProgressBar from '../components/CircularBar';
 import { fetchCab } from '../redux/slices/fetchCabSlice';
 
 const CabShow = () => {
@@ -19,7 +23,7 @@ const CabShow = () => {
   if (loading) return <div>Loading...</div>;
   console.log(selectedCab);
   return (
-    <main className="p-8  pt-24 relative h-full">
+    <main className="p-10  relative h-full">
       <button
         className="absolute bottom-12 left-0 transform
           p-5 pl-16 rounded-r-full bg-lime-400"
@@ -28,40 +32,59 @@ const CabShow = () => {
       >
         <BiLeftArrow className="text-white text-xl" />
       </button>
-      <section className="grid grid-cols-2">
-        <section className="flex justify-end">
+      <section className="grid pl-10 grid-cols-2">
+        <section className="flex justify-end  pt-24">
           <img
             src={selectedCab.imageUrl}
             alt={selectedCab.model}
-            className="aspect-square w-[70%] rounded-lg"
+            className="aspect-square w-[80%] rounded-lg"
           />
         </section>
-        <section className="flex flex-col items-end px-6">
-          <header>
+        <section className="flex flex-col gap-8 items-end px-6">
+          <header className="flex flex-col items-end">
             <h1 className="font-bold text-2xl">{selectedCab.model}</h1>
             <p>{selectedCab.transmission}</p>
           </header>
-          <section className="flex w-full flex-col odd:bg-gray-200 items-end space-y-4">
-            <span className="flex text-xl w-[60%] justify-between">
-              <h4>Rental Price:</h4>
-              <p className=" font-bold">{selectedCab.rentalPrice}</p>
+          <section className="flex w-full flex-col  items-end space-y-2">
+            <span className="flex font-semibold rounded-sm text-lg w-[60%] justify-between px-4 tracking-wider py-2 bg-gray-200">
+              <h4>Rental Price</h4>
+              <p className=" ">{`$${selectedCab.rentalPrice}/2KM`}</p>
             </span>
-            <span className="flex text-xl w-[60%]  justify-between">
+            <span className="flex font-semibold rounded-md text-lg w-[60%] justify-between px-4 tracking-wider py-1 ">
               <h4>Manufacturer:</h4>
               <p className=" font-bold">{selectedCab.manufacturer}</p>
             </span>
-            <span className="flex text-xl w-[60%]  justify-between">
+            <span className="flex font-semibold rounded-sm text-lg w-[60%] justify-between px-4 tracking-wider py-2 bg-gray-200">
               <h4>Capacity:</h4>
               <p className=" font-bold">{selectedCab.seatingCapacity}</p>
             </span>
-            <span className="flex text-xl w-[60%]  justify-between">
+            <span className="flex font-semibold rounded-md text-lg w-[60%] justify-between px-4 tracking-wider py-1">
               <h4>Body Type:</h4>
               <p className=" font-bold">{selectedCab.bodyType}</p>
             </span>
-            <span className="flex text-xl w-[60%] justify-between">
+            <span className="flex font-semibold rounded-sm text-lg w-[60%] justify-between px-4 tracking-wider py-2 bg-gray-200">
               <h4>Transmission:</h4>
               <p className=" font-bold">{selectedCab.transmission}</p>
             </span>
+            <span className="flex rounded-sm text-lg w-full justify-end px-4 tracking-wider py-2">
+              <h4 className="flex m-0 gap-1">
+                <span className="font-bold text-xl">
+                  {`${selectedCab.discount}% `}
+                  discount
+                </span>
+                on distances over 2KM.
+              </h4>
+            </span>
+          </section>
+          <section className="flex flex-col items-end gap-8">
+            <Link to="/cabs" className="flex items-center">
+              <p className="font-semibold tracking-widest">
+                {' '}
+                Checkout other Models
+              </p>
+              <MdOutlineKeyboardArrowRight className="text-2xl animate-pulse" />
+            </Link>
+            <MultiColorProgressBar />
           </section>
           <section>
             <Link to="/reservations/new">
