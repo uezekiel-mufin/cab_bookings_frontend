56126caf4b1f00e8dae7d1ba2727bc3681fedb40
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-06-15 12:07:35 +0100

    Add styling to cab index page

--- src/components/Cab.jsx
+++ src/components/Cab.jsx
@@ -6,33 +6,35 @@ import { FaMediumM, FaTwitter, FaInstagram } from 'react-icons/fa';
 const Cab = ({ cab }) => (
   <li
     key={cab.model}
-    className="flex flex-col gap-6 items-center transition-all duration-300 ease-linear"
+    className="grid grid-rows-2 items-start gap-4 transition-all duration-300 ease-linear"
   >
-    <Link to={`/cabs/${cab.id}`} className="flex flex-col gap-8 items-center">
+    <Link to={`/cabs/${cab.id}`} className="flex flex-col gap-4 items-center">
       <img
         src={cab.image_url}
         alt={cab.model}
         className="rounded-full aspect-square w-60 "
       />
+    </Link>
+    <section className="flex flex-col gap-4 items-center justify-evenly">
       <h2 className="font-extrabold text-[#645858] text-xl tracking-wider">
         {cab.model}
       </h2>
-    </Link>
-    <hr className="border-dotted w-1/2 font-bold text-2xl border-[3px]" />
-    <p className="text-[#807e80]">
-      {`${cab.description.substring(0, 150)}...`}
-    </p>
-    <div className="flex gap-4">
-      <span className="rounded-full border p-2">
-        <FaTwitter className="text-xl  text-[#a7a4a7]  cursor-pointer  " />
-      </span>
-      <span className="rounded-full border p-2">
-        <FaInstagram className="text-xl text-[#a7a4a7] cursor-pointer  " />
-      </span>
-      <span className="rounded-full border p-2">
-        <FaMediumM className="text-xl  text-[#a7a4a7]  cursor-pointer  " />
-      </span>
-    </div>
+      <hr className="border-dotted w-full font-bold text-2xl border-[3px]" />
+      <p className="text-[#807e80]">
+        {`${cab.description.substring(0, 100)}...`}
+      </p>
+      <div className="flex gap-4">
+        <span className="rounded-full border p-2">
+          <FaTwitter className="text-xl  text-[#a7a4a7]  cursor-pointer  " />
+        </span>
+        <span className="rounded-full border p-2">
+          <FaInstagram className="text-xl text-[#a7a4a7] cursor-pointer  " />
+        </span>
+        <span className="rounded-full border p-2">
+          <FaMediumM className="text-xl  text-[#a7a4a7]  cursor-pointer  " />
+        </span>
+      </div>
+    </section>
   </li>
 );
 
