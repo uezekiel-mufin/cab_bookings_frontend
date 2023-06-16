09aa33a9dc6270c9131a2e4fad1fd514baa2b110
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-06-16 11:15:57 +0300

    Add sign-up inputs

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Signup = () => {
-  console.log('this is the signup page');
-  return <div>Signup</div>;
+const SignUpComponent = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      setPasswordError('Passwords do not match');
+      return;
+    }
+
+    // Here, you can perform sign-up logic, such as making an API request to register the user
+    console.log('Signing up:', {
+      firstName, lastName, email, password,
+    });
+    // Reset form fields
+    setFirstName('');
+    setLastName('');
+    setEmail('');
+    setPassword('');
+    setConfirmPassword('');
+    setPasswordError('');
+  };
+
+  return (
+    <form onSubmit={handleSignUp}>
+      <h2>Sign Up</h2>
+      <label htmlFor="firstName">
+        First Name:
+        <input
+          type="text"
+          id="firstName"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          required
+        />
+      </label>
+      <label htmlFor="lastName">
+        Last Name:
+        <input
+          type="text"
+          id="lastName"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          required
+        />
+      </label>
+      <label htmlFor="email">
+        Email:
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </label>
+      <label htmlFor="password">
+        Password:
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+      <label htmlFor="confirmPassword">
+        Confirm Password:
+        <input
+          type="password"
+          id="confirmPassword"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          required
+        />
+      </label>
+      {passwordError && <p>{passwordError}</p>}
+      <button type="submit">Sign Up</button>
+    </form>
+  );
 };
 
-export default Signup;
+export default SignUpComponent;
