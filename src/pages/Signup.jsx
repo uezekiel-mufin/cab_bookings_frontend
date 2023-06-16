f8960e9052029d61ea3547cc71324c176b1fb236
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-06-16 16:04:29 +0300

    Update signup page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 
 const SignUpComponent = () => {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
+  const [Name] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
@@ -18,11 +17,10 @@ const SignUpComponent = () => {
 
     // Here, you can perform sign-up logic, such as making an API request to register the user
     console.log('Signing up:', {
-      firstName, lastName, email, password,
+      Name, email, password,
     });
     // Reset form fields
-    setFirstName('');
-    setLastName('');
+    Name('');
     setEmail('');
     setPassword('');
     setConfirmPassword('');
@@ -30,61 +28,60 @@ const SignUpComponent = () => {
   };
 
   return (
-    <form onSubmit={handleSignUp}>
-      <h2>Sign Up</h2>
-      <label htmlFor="firstName">
-        First Name:
-        <input
-          type="text"
-          id="firstName"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-          required
-        />
-      </label>
-      <label htmlFor="lastName">
-        Last Name:
-        <input
-          type="text"
-          id="lastName"
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-          required
-        />
-      </label>
-      <label htmlFor="email">
-        Email:
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </label>
-      <label htmlFor="password">
-        Password:
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <label htmlFor="confirmPassword">
-        Confirm Password:
-        <input
-          type="password"
-          id="confirmPassword"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          required
-        />
-      </label>
-      {passwordError && <p>{passwordError}</p>}
-      <button type="submit">Sign Up</button>
-    </form>
+    <div className="py-16 space-y-8 bg-lime-50 h-screen overflow-auto">
+      <h2 className="flex justify-center text-3xl md:text-3xl text-lime-800 font-bold">Sign Up</h2>
+      <form onSubmit={handleSignUp} className="w-full grid grid-cols-1 gap-4 mb-20 px-4 md:px-10 text-lime-800 lg:px-20 xl:px-40 md:gap-x-4">
+        <label htmlFor="Name">
+          Name:
+          <input
+            type="text"
+            id="Name"
+            value={Name}
+            onChange={(e) => Name(e.target.value)}
+            required
+          />
+        </label>
+
+        <label htmlFor="email">
+          Email:
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </label>
+        <label htmlFor="password">
+          Password:
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        <label htmlFor="confirmPassword">
+          Confirm Password:
+          <input
+            type="password"
+            id="confirmPassword"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+        </label>
+        {passwordError && <p>{passwordError}</p>}
+        <button
+          type="submit"
+          className="bg-lime-700
+            w-full flex gap-2 items-center transition-all duration-300 text-white rounded-md justify-center text-2xl px-8 py-2"
+        >
+          Sign Up
+        </button>
+      </form>
+    </div>
   );
 };
 
