1e14fc2ae7f5b7e1a658e5be604582edc0fd9ed3
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-06-16 16:11:16 +0100

    A navbar and responsiveness of navbar for small device

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet, useNavigate, useLocation } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 // import Cookies from 'js-cookie';
 import { AiOutlineMenuUnfold } from 'react-icons/ai';
@@ -12,6 +12,7 @@ import { toggleMenu } from './redux/slices/menuSlice';
 
 function App() {
   const navigate = useNavigate();
+  const location = useLocation();
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.user);
   const isMenuOpen = useSelector((state) => state.menu.isMenuOpen);
@@ -29,16 +30,7 @@ function App() {
   }, [navigate, user]);
 
   return (
-    <div className="grid  grid-cols-1 md:grid-cols-7 lg:grid-cols-11 overflow-auto divide-x-2 divide-gray-200">
-      {!isMenuOpen && (
-        <button
-          type="button"
-          className="flex p-4 md:hidden w-auto fixed z-0 left-0 top-0  "
-          onClick={() => dispatch(toggleMenu())}
-        >
-          <AiOutlineMenuUnfold className="text-3xl text-lime-800" />
-        </button>
-      )}
+    <div className="grid relative grid-cols-1 md:grid-cols-7 lg:grid-cols-11 overflow-auto divide-x-2 divide-gray-200">
       <section className="hidden w-full md:flex md:col-span-2 max-h-screen lg:col-span-2">
         <ToastContainer position="top-center" />
         <Navbar />
@@ -48,7 +40,22 @@ function App() {
           <Navbar />
         </section>
       )}
-      <section className=" md:col-span-5 py-20 md:py-10 lg:py-0 pt-4 overflow-auto bg-lime-50 lg:col-span-9">
+      <section className="relative md:col-span-5 py-20 md:py-10 lg:py-0 pt-0 overflow-auto bg-lime-50 lg:col-span-9">
+        {!isMenuOpen && (
+          <button
+            type="button"
+            className="flex md:hidden w-auto absolute z-0 left-0 top-0 p-4"
+            onClick={() => dispatch(toggleMenu())}
+          >
+            <AiOutlineMenuUnfold
+              className={`text-4xl  ${
+                location.pathname === '/reservations/new'
+                  ? 'text-lime-50'
+                  : 'text-lime-800'
+              }`}
+            />
+          </button>
+        )}
         <Outlet />
       </section>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,16 @@
 /* eslint-disable object-curly-newline */
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { FaMediumM, FaTwitter, FaInstagram, FaFacebook } from 'react-icons/fa';
 import { MdOutlineClose } from 'react-icons/md';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { toggleMenu } from '../redux/slices/menuSlice';
 
 const Navbar = () => {
   const [activeLink, setActiveLink] = useState(1);
   const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.user);
+  const location = useLocation();
   const navLinks = [
     {
       id: 1,
@@ -40,12 +42,18 @@ const Navbar = () => {
 
   const handleLinkClick = (id) => {
     setActiveLink(id);
+    dispatch(toggleMenu());
   };
 
+  useEffect(() => {
+    const link = navLinks.find((item) => item.path === location.pathname);
+    setActiveLink(link.id);
+  }, [location.pathname]);
+
   return (
-    <div className="max-h-screen z-10 flex flex-col w-full justify-between py-6">
-      <section className="flex justify-between gap-12 pl-6 flex-col">
-        <div className="flex justify-between items-center w-full">
+    <div className="max-h-screen z-10 flex pl-4 flex-col w-full justify-between py-6">
+      <section className="flex justify-between gap-12  flex-col">
+        <div className="flex justify-between px-4 items-center w-full">
           <h1 className="text-indigo-500 text-3xl  font-extrabold">CarBooky</h1>
           <button
             type="button"
@@ -56,14 +64,14 @@ const Navbar = () => {
           </button>
         </div>
         <nav>
-          <ul className="flex flex-col  text-lg font-bold text-[#6d6b6b]">
+          <ul className="flex flex-col mt-8 text-lg font-bold text-lime-800">
             {navLinks.map((link) => (
               <Link to={link.path} key={link.id}>
                 <li
                   onClick={() => handleLinkClick(link.id)}
                   className={`${
                     activeLink === link.id && 'bg-lime-600 text-white'
-                  } px-4 py-3 transition-all duration-300 cursor-pointer ease-linear`}
+                  } px-4 py-2 transition-all duration-300 cursor-pointer ease-linear`}
                   role="presentation"
                 >
                   {link.name}
@@ -71,6 +79,13 @@ const Navbar = () => {
               </Link>
             ))}
           </ul>
+          <div className="flex px-4 justify-start">
+            {user && (
+              <button type="button" className="text-lime-800 font-bold text-lg">
+                Logout
+              </button>
+            )}
+          </div>
         </nav>
       </section>
       <section className="space-y-3 flex flex-col items-center">
