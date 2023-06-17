067a1659db18a8443c5a574c7574ede8bbe592ac
Outkarm <jkpordje@gmail.com>
2023-06-17 08:54:49 +0000

    add link from react router dom

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 
 const Login = () => {
