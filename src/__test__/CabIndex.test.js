088275aa72dd8b7bc7f383fad12062522b5188b3
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-06-19 17:10:45 +0100

    Fix linters

--- src/__test__/CabIndex.test.js
+++ src/__test__/CabIndex.test.js
@@ -7,25 +7,8 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import CabIndex from '../pages/CabIndex';
 import store from '../redux/store/store';
-import Cab from '../components/Cab';
 
 describe('CabIndex', () => {
-  const testCab = {
-    id: 7,
-    manufacturer: 'Mercedes-Benz',
-    model: 'Mercedes-Benz C-Class',
-    transmission: 'Automatic',
-    rental_price: 100,
-    discount: 5,
-    engine_type: '5L V8',
-    image_url:
-      'https://images.unsplash.com/photo-1681170368080-f240d1216904?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fE1lcmNlZGVzJTIwQmVueiUyMEMlMjBDbGFzc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=400&q=60',
-    seating_capacity: 5,
-    body_type: 'Sedan',
-    description:
-      'The Mercedes-Benz C-Class is a luxury sedan that combines elegance, performance, and advanced technology. With its automatic transmission, you can enjoy a refined and comfortable driving experience. The C-Class offers seating for up to five passengers, providing a luxurious travel experience.',
-  };
-
   it('renders', () => {
     const tree = TestRenderer.create(
       <Provider store={store}>
@@ -64,4 +47,22 @@ describe('CabIndex', () => {
       expect(subHead).toBeInTheDocument();
     });
   });
+
+  // test('renders CabIndex and checks for existence of data', () => {
+  //   render(
+  //     <Provider store={store}>
+  //       <BrowserRouter>
+  //         <Cab cab={testCab} />
+  //       </BrowserRouter>
+  //     </Provider>,
+  //   );
+
+  //   const cabTitle = screen.getByText(
+  //     (content, element) =>
+  //       element.tagName.toLowerCase() === 'h2' &&
+  //       content.includes(testCab.model),
+  //   );
+
+  //   expect(cabTitle).toBeInTheDocument();
+  // });
 });
